90359436fd122256c1a80c4b22d42b5eb8e5f8a2
Julian4047 <81939824+Julian4047@users.noreply.github.com>
2021-07-15 19:54:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ function myFunctionCreate() {
   var btn = document.createElement("div");
   btn.innerHTML = text.value;
   document.body.appendChild(btn);
+var input = text.value; 
+ btn.classList.add(input);
 }
 
 function myFunctionRemove() {
-  var myobj = document.querySelector("div");
+var input = text.value;
+  var myobj = document.querySelector("."+input);
   myobj.remove();
 }
+
 //2ndOption
 //function myFunctionCreate() {
 //var btn = document.createElement("div");
@@ -20,3 +24,15 @@ function myFunctionRemove() {
 //var myobj = document.querySelector(".input");
 //myobj.remove();
 //} 
+
+//3ndOption
+//function myFunctionCreate() {
+//var btn = document.createElement("div");
+//btn.innerHTML = text.value;
+//document.body.appendChild(btn);
+//}
+
+//function myFunctionRemove() {
+//var myobj = document.querySelector("div");
+//myobj.remove();
+//}
