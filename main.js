a12c9819c1006e9e399e3a8fe33e0e9d94071a52
Julian4047 <81939824+Julian4047@users.noreply.github.com>
2021-07-15 19:39:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ function myFunctionRemove() {
   var myobj = document.querySelector("div");
   myobj.remove();
 }
+//2ndOption
+//function myFunctionCreate() {
+//var btn = document.createElement("div");
+//btn.innerHTML = text.value;
+//document.body.appendChild(btn);
+//btn.classList.add("todo");
+//}
+
+//function myFunctionRemove() {
+//var myobj = document.querySelector(".todo");
+//myobj.remove();
+//} 
